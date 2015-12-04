68a912943ac1a1aeea48439448621f0658a96d5a
jeanfabre <contact@fabrejean.net>
2015-12-04 09:01:43 +0300

    Initial commit

---
+++ Assets/LoadMainScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadMainScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		Application.LoadLevelAdditive("My Game Scene");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/MyComponent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyComponent : MonoBehaviour {
+
+	public bool MyBool = false;
+
+	public float MyFloat = 0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		MyFloat ++;
+
+		if (! MyBool && MyFloat > 20f)
+		{
+			MyBool = true;
+		}
+	}
+}
